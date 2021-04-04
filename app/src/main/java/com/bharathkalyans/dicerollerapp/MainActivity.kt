d2979915949903d4ebea5509b8a73d701e641d73
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-04 16:03:39 +0530

    Created the Image View Added the Dices and Modified the Code to work with Images(Vectors.)

--- app/src/main/java/com/bharathkalyans/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/bharathkalyans/dicerollerapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -23,19 +24,24 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
 
-            Toast.makeText(this, " Random Number Generated", Toast.LENGTH_SHORT)
         }
     }
 
 
     private fun rollDice() {
 
-        var resultText: TextView = findViewById(R.id.resultText)
+        val diceImage: ImageView = findViewById(R.id.diceImage)
 
-        var randomNumber = Random.nextInt(6) + 1
+        val randomNumber = Random.nextInt(6) + 1
 
-        resultText.text = randomNumber.toString()
-
-
+        val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
